843a94836899ecd390d4173a690d2653e5c452fe
CasTeTRU <kongalkin@gmail.com>
2025-11-03 00:45:41 +0300

    ДЗ Спринт 21 1) Добавляем зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.navigation.safe.args)
+    kotlin("kapt")
 }
 
 android {
@@ -75,5 +76,7 @@ dependencies {
     // Koin
     implementation("io.insert-koin:koin-android:3.3.0")
 
-
+    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+    kapt("androidx.room:room-compiler:2.6.0")
 }
